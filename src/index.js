493afb02f37cbd2f6bb0b509a7967db5aa0167c0
Dawid Bazyluk <dawid.bazyluk@gmail.com>
2023-02-02 12:35:41 +0100

    update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import './css/styles.css';
 import axios from 'axios';
 import Notiflix from 'notiflix';
+
+const searchForm = document.querySelector('#search-form');
+
+console.log(searchForm.elements);
